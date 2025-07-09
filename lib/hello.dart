a6e6119b592d8ab514c4737ad4e81a6fe8eb81ab
Yash Virsodiya <virsodiyayash4@gmail.com>
2025-07-09 18:34:34 +0530

    Hello

---
+++ lib/hello.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+
+class Hello extends StatelessWidget {
+  const Hello({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello world"),
+      ),
+      body: Center(
+        child : Text("Hello World" , style: TextStyle(fontSize: 40),)
+      ),
+    );
+  }
+}
